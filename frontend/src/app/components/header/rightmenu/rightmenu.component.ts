12c9015356d188687bb2879c6d220f2634d4d864
juanxo13597 <juanxo97master@gmail.com>
2023-03-09 20:04:08 +0100

    menu de usuario y logout

--- frontend/src/app/components/header/rightmenu/rightmenu.component.ts
+++ frontend/src/app/components/header/rightmenu/rightmenu.component.ts
@@ -1,10 +1,20 @@
+import { AppState } from './../../../state/app.state';
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as actionsAuth from '../../../state/auth/auth.actions';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-rightmenu',
   templateUrl: './rightmenu.component.html',
-  styleUrls: ['./rightmenu.component.scss']
+  styleUrls: ['./rightmenu.component.scss'],
 })
 export class RightmenuComponent {
+  constructor(private store: Store<AppState>, private router: Router) {}
 
+  logout() {
+    localStorage.removeItem('user');
+    this.store.dispatch(actionsAuth.logout());
+    this.router.navigate(['/users/login']);
+  }
 }
